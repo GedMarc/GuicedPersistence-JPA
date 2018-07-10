43189992e82a6b1b8f76fe28d205b2d1c6d500e7
GedMarc <ged_marc@hotmail.com>
2018-07-10 18:50:18 +0200

    Add classifiers for the built jars in their JREs

--- src/test/java/com/jwebmp/guicedpersistence/jpa/GuicedPersistenceBindingTest.java
+++ src/test/java/com/jwebmp/guicedpersistence/jpa/GuicedPersistenceBindingTest.java
@@ -2,6 +2,7 @@ package com.jwebmp.guicedpersistence.jpa;
 
 import com.jwebmp.guicedinjection.GuiceContext;
 import com.jwebmp.logger.LogFactory;
+import com.jwebmp.logger.logging.LogColourFormatter;
 import org.junit.jupiter.api.Test;
 
 import java.util.logging.Level;
@@ -12,6 +13,7 @@ class GuicedPersistenceBindingTest
 	public void testMe()
 	{
 		LogFactory.configureConsoleSingleLineOutput(Level.FINE);
+		LogColourFormatter.setRenderBlack(false);
 		GuiceContext.inject();
 	}
 }
