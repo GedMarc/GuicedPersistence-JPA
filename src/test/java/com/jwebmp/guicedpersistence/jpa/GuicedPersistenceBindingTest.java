bd52eb65bce127c4c4ecfd80d8ddedb1936f06e8
GedMarc <ged_marc@hotmail.com>
2018-09-11 00:22:01 +0200

    Lint

--- src/test/java/com/jwebmp/guicedpersistence/jpa/GuicedPersistenceBindingTest.java
+++ src/test/java/com/jwebmp/guicedpersistence/jpa/GuicedPersistenceBindingTest.java
@@ -1,19 +1,24 @@
 package com.jwebmp.guicedpersistence.jpa;
 
 import com.jwebmp.guicedinjection.GuiceContext;
+import com.jwebmp.guicedpersistence.db.AsyncPostStartup;
 import com.jwebmp.logger.LogFactory;
 import com.jwebmp.logger.logging.LogColourFormatter;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 class GuicedPersistenceBindingTest
 {
 	@Test
-	public void testMe()
+	public void testMe() throws InterruptedException
 	{
 		LogFactory.configureConsoleSingleLineOutput(Level.FINE);
 		LogColourFormatter.setRenderBlack(false);
 		GuiceContext.inject();
+
+		AsyncPostStartup.getDbAutoStartupExecutors()
+		                .awaitTermination(10, TimeUnit.MINUTES);
 	}
 }
