698a078e82670cf28d7bd2832992b5973df66932
GedMarc <ged_marc@hotmail.com>
2018-11-01 01:10:35 +0200

    Transitive dependencies don't filter down, opening the requested module seems to work great

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -5,14 +5,21 @@ import com.jwebmp.guicedpersistence.services.ITransactionHandler;
 
 module com.jwebmp.guicedpersistence.jpa {
 	exports com.jwebmp.guicedpersistence.jpa;
+	requires aopalliance;
+	requires java.logging;
+	requires java.persistence;
+	requires com.jwebmp.guicedinjection;
+	requires com.google.guice;
+	requires com.jwebmp.guicedpersistence;
+	requires com.jwebmp.logmaster;
+	requires java.sql;
+	requires java.transaction;
+	requires com.google.guice.extensions.persist;
 
-	requires transitive com.jwebmp.guicedpersistence;
-
-	exports com.jwebmp.guicedpersistence.jpa.implementations to com.jwebmp.guicedinjection;
+	exports com.jwebmp.guicedpersistence.jpa.implementations to com.jwebmp.guicedinjection, com.jwebmp.examples.demos.homepage;
 
 	provides ITransactionHandler with JPAAutomatedTransactionHandler;
 	provides IGuiceDefaultBinder with JPAGuicedPersistenceInterceptionBinding;
-
 	opens com.jwebmp.guicedpersistence.jpa to com.fasterxml.jackson.databind, com.google.guice;
 	opens com.jwebmp.guicedpersistence.jpa.implementations to com.fasterxml.jackson.databind, com.google.guice;
 }
