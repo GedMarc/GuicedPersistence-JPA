c78efbebf4c8a7d80dc32469d527d527aa9e0a81
GedMarc <ged_marc@hotmial.com>
2020-03-02 10:28:12 +0200

    Module Linting

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -17,9 +17,6 @@ module com.guicedee.guicedpersistence.jpa {
 
 	provides com.guicedee.guicedpersistence.services.ITransactionHandler with com.guicedee.guicedpersistence.jpa.implementations.JPAAutomatedTransactionHandler;
 
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.guicedee.guicedpersistence.jpa.implementations.JPAModuleExclusions;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.guicedee.guicedpersistence.jpa.implementations.JPAModuleExclusions;
-
 	opens com.guicedee.guicedpersistence.jpa to com.fasterxml.jackson.databind, com.google.guice;
 	opens com.guicedee.guicedpersistence.jpa.implementations to com.fasterxml.jackson.databind, com.google.guice;
 }
